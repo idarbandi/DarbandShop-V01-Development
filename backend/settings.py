cefda3043736a392f535b9cb06c375d6431057ec
idarbandi <darbandidr99@gmail.com>
2023-04-19 20:06:13 +0330

    added more Styling & Functionality

--- backend/settings.py
+++ backend/settings.py
@@ -44,6 +44,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "store.views.categories"
             ],
         },
     },
@@ -82,6 +83,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
